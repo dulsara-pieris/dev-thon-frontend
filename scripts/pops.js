6ced712f4e38659127b2978ab0a580a295dd38af
dulsara-pieris <dulsara.synapsnex@gmail.com>
2026-02-07 12:52:38 +0530

    pop up

---
+++ scripts/pops.js
@@ -0,0 +1,17 @@
+const openBtn = document.querySelector('.open-user');
+const popup = document.querySelector('.user-overlay');
+const closeBtn = document.querySelector('.close-user');
+
+openBtn.addEventListener('click', () => {
+        popup.style.display = 'flex';
+});
+
+closeBtn.addEventListener('click', () => {
+        popup.style.display = 'none';
+});
+
+popup.addEventListener('click', (e) => {
+        if (e.target === popup) {
+                popup.style.display = 'none';
+        }
+});
