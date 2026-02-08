0a280b800d65d6f7ab091e349f1deb93d151931b
dulsara-pieris <dulsara.synapsnex@gmail.com>
2026-02-08 11:30:38 +0530

    commit before the bug check

--- scripts/history.js
+++ scripts/history.js
@@ -1,19 +1,3 @@
-const slider = document.getElementById("slide-con");
-const links = document.querySelectorAll("a[data-target]");
-
-links.forEach(link => {
-        link.addEventListener("click", (e) => {
-                e.preventDefault();
-                const target = link.getAttribute("data-target");
-
-                if (target === "login") {
-                        slider.style.transform = "translateX(-100vw)";
-                } else if (target === "signup") {
-                        slider.style.transform = "translateX(0)";
-                }
-        });
-});
-
 const videosContainer = document.querySelector(".videos");
 
 
